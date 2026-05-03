d09a62c9e49efb4fefcf90779681dfad74394c75
rajas22311493-ux <rajas.22311493@viit.ac.in>
2026-05-03 17:37:25 +0530

    Update App.jsx

--- src/components/src/components/src/redux/src/src/App.jsx
+++ src/components/src/components/src/redux/src/src/App.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductList from "./components/ProductList";
 
 function App() {
+  const [showProducts, setShowProducts] = useState(false);
+
+  const handleGetStartedClick = () => {
+    setShowProducts(true);
+  };
+
   return (
-    <div>
-      <h1>Paradise Nursery 🌿</h1>
-      <div style={{ textAlign: "center" }}>
-        <button>Get Started</button>
-      </div>
-      <ProductList />
+    <div className="background-image" style={{ textAlign: "center", paddingTop: "50px" }}>
+      
+      {/* REQUIRED TEXT (must match exactly) */}
+      <h1>Welcome to Paradise Nursery</h1>
+
+      {/* BUTTON WITH FUNCTION */}
+      {!showProducts && (
+        <button onClick={handleGetStartedClick}>
+          Get Started
+        </button>
+      )}
+
+      {/* SHOW PRODUCTS AFTER CLICK */}
+      {showProducts && <ProductList />}
+
     </div>
   );
 }
