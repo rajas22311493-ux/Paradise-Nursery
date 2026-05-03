ee3149b010fe65bfa7c3b642ff78a7445059f19a
rajas22311493-ux <rajas.22311493@viit.ac.in>
2026-05-03 17:23:39 +0530

    Create App.jsx

---
+++ src/components/src/components/src/redux/src/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div>
+      <h1>Paradise Nursery 🌿</h1>
+      <button>Get Started</button>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
