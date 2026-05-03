305c3869dd647a109d242e6a1e4728d728ad86bb
rajas22311493-ux <rajas.22311493@viit.ac.in>
2026-05-03 17:20:49 +0530

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery provides a wide range of indoor plants to beautify your home.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
